e89c2689c60d0fb209ea722962b536534c0d12f0
furenku <furenku@gmail.com>
2017-01-05 00:21:12 -0600

    infrastroctrochor

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+
+    <link rel="stylesheet" href="recursos/style_utils/css/style_utils.css">
+
+    <link rel="stylesheet" href="css/app.css">
+
+  </head>
+  <body>
+
+   <div class="row expanded h-100-v v-center text-right">
+      <div class="columns h-a text-right p-1">
+         Jorge Ramírez
+      </div>
+   </div>
+
+
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/what-input/dist/what-input.js"></script>
+    <script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
+    <script src="recursos/js_utils/dist/js/js_utils.min.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).foundation();
+
+u = new Utils();
+
+$(document).ready(function(){
+
+   u.addWindowResizeFunction( u.verticalCenter )
+
+
+});
